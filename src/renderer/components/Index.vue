b6e4ec9aa7c0a194b48f64fdbb0ce12f19ebf517
Duct-and-rice <nikuinemitsubishi@gmail.com>
2017-12-01 01:02:44 +0900

    add Index.vue

---
+++ src/renderer/components/Index.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+    #wrapper
+        | a
+</template>
+
+<script>
+  export default {
+      name: 'index',
+      methods: {
+          open (link) {
+              this.$electron.shell.openExternal(link)
+          }
+      }
+  }
+</script>
+
+<style></style>
