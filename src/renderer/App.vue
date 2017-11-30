6eb7527d62646d29d9c7e75d90c4b1293bac39e7
Duct-and-rice <nikuinemitsubishi@gmail.com>
2017-11-30 15:37:19 +0900

    initialize

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+      name: 'drop-me-baby'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
